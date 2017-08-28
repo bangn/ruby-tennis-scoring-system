360e2d42c100b716e1103cf7857bd6913e7b31da
bangn <thebangnguyen@gmail.com>
2017-08-29 07:16:52 +1000

    Use forwardable library to forward methods.
    Forward point_won_by and score of Match to Referee.

--- lib/match.rb
+++ lib/match.rb
@@ -1,20 +1,19 @@
 # frozen_string_literal: true
 
+require 'forwardable'
+
 require 'referee'
 require 'player'
 
 class Match
+  extend Forwardable
+
+  def_delegator :@referee, :point_won_by
+  def_delegator :@referee, :score
+
   def initialize(player1, player2)
     @player1 = Player.new(player1, 0, 0)
     @player2 = Player.new(player2, 0, 0)
     @referee = Referee.new(@player1, @player2)
-  end
-
-  def point_won_by(player)
-    @referee.point_won_by(player)
-  end
-
-  def score
-    @referee.score
   end
 end
